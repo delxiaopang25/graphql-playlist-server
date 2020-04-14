f09beac33deced300134d42516e136485fae6128
Kevin Qin <delxiaopang@gmail.com>
2020-04-13 17:56:33 -0700

    Server side for graphql-playlist website

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./scheme/scheme');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/graphql-playlist',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open',() => {
+    console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({schema: schema,graphiql:true}));
+
+app.listen(4000, () => {
+    console.log("server starts and listen at 4000");
+});
